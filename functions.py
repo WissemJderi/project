fdc407cdd729333aab8d8d3c1f77ab450e2e65ac
Wissem <asqr43641@gmail.com>
2025-07-24 18:53:40 +0100

    feat: see a single note

--- functions.py
+++ functions.py
@@ -1,28 +1,44 @@
 import json
 import os
 
-def add_note(id, title, date, content):
-    return {
-        "id": id,
-        "title": title,
-        "date": date,
-        "content": content
-    }
 
-def view_all_notes():
-    print("Working From view_all_notes")
+def add_note(notes_len):
+    title = input("Enter a title: ")
+    content = input("Enter note content: ")
+    id = notes_len + 1
+    time_created = "20-02-2025"
 
-def view_one_note():
-    print("Working From view_one_note")
+    return {"id": id, "title": title, "date": time_created, "content": content}
+
+
+def view_all_notes(notes):
+    print(f"Notes created: {len(notes)} ")
+    for note in notes:
+        
+        print("----------")
+        print(
+            f"Title: {note["title"]}\nId: {note["id"]}\nContent: {note["content"]}\nDate: {note["date"]}"
+        )
+        # TODO Add colorama
+        print("----------")
+
+
+def view_one_note(notes):
+    id = input("See a note by it's id")
+    print(notes[id + 1])
+
 
 def edit_note():
     print("Working From edit_note")
 
+
 def delete_note():
     print("Working From delete_note")
 
+
 def exit_prog():
     print("Working From exit_prog")
+
 
 def notes_len():
     print("Working From notes_len")
--- main.py
+++ main.py
@@ -16,25 +16,20 @@ import os
 def main():
     notes = load_notes()
     while True:
-        print("1. Add a note \n2. Show all notes \n3. Delete a note \n4. Exit")
+        print(
+            "1. Add a note \n2. View all notes \n3. View one note \n4. Edit a note \n5. Delete a note \n6. Exit"
+        )
         user_input = input("Enter a number: ")
-        if user_input != "4":
+        if user_input != "6":
             if user_input == "1":
-                notes.append(
-                    add_note(
-                        5, "finish the project", "20-3-2025", "finish it in 3 days"
-                    )
-                )
+                notes.append(add_note(len(notes)))
                 save_notes(notes)
+                print("Your note has been saved!")
             elif user_input == "2":
-                for note in notes:
-                    # print(note)
-                    print("----------")
-                    print(
-                        f"Title: {note["title"]}\nId: {note["id"]}\nContent: {note["content"]}\nDate: {note["date"]}",
-                    )
-                    # TODO Add colorama
-                    print("----------")
+                # print(note)
+                view_all_notes(notes)
+            elif user_input == "3":
+                view_one_note(notes)
             continue
         else:
             sys.exit()
